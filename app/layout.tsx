924a1e5e03c0ed9ddcbad806f5d7b3791c76acdd
weasy1as <habdu21@student.sdu.dk>
2026-05-26 14:45:02 +0200

    added smooth scrooling

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.className} antialiased`}>
+      <body className={`${geistSans.className} antialiased scroll-smooth`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
